bc807992dff8aa46ffeecb8ca0c85d964d57b9d1
jakub5016 <me.jakub.gil@gmail.com>
2025-03-16 21:51:43 +0100

    New file structure

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+module "bucket" {
+  source    = "./modules/bucket"
+}
+
+module "backend" {
+  source    = "./modules/backend"
+  depends_on = [module.bucket]
+}
+
+module "frontend" {
+  source    = "./modules/frontend"
+  depends_on = [module.backend]
+  backend_url = module.backend.backend_url
+}
