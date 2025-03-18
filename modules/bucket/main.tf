50d161e373dbf7460ba417a0d2ae3aa852fd3ab6
jakub5016 <me.jakub.gil@gmail.com>
2025-03-18 12:43:22 +0100

    Working bucket

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,17 +1,19 @@
-resource "aws_s3_bucket" "main_bucket" {
-  bucket = "main_bucket"
+resource "aws_s3_bucket" "my_docker_bucket" {
+  bucket = "awsbuckera"
 }
 
 resource "aws_s3_object" "backend_app_zip" {
-  bucket = aws_s3_bucket.main_bucket.id
+  bucket = aws_s3_bucket.my_docker_bucket.id
   key    = "backend-app.zip"
-  source = "./project/backend/backend-app.zip"
+  source = "~/environment/aws/project/backend/backend-app.zip"
+  etag = filemd5("~/environment/aws/project/backend/backend-app.zip")
 }
 
 resource "aws_s3_object" "frontend_app_zip" {
-  bucket = aws_s3_bucket.main_bucket.id
+  bucket = aws_s3_bucket.my_docker_bucket.id
   key    = "frontend-app.zip"
-  source = "./project/fronted/frontend-app.zip"
+  source = "~/environment/aws/project/frontend/frontend-app.zip"
+  etag = filemd5("~/environment/aws/project/frontend/frontend-app.zip")
 }
 
 output "bucket_name" {
